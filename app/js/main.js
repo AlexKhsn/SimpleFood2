f158211be3ed75824bfc5a3636ba348dfa79ea2a
AlexKhsn <alexkhomishyn@gmail.com>
2023-12-28 05:06:56 +0100

    added POPULAR

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function(){
     
+    
+    
+    $('.popular__categories-btn').on('click', function(){ 
+        $('.popular__categories-btn').removeClass('popular__categories-btn--active');
+        $(this).addClass('popular__categories-btn--active');
+    });
 });
+
+
+
+var mixer = mixitup('.popular__items');
