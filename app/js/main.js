8db045a999159b307030edc1a066b426460cda60
AlexKhsn <alexkhomishyn@gmail.com>
2023-12-30 05:36:02 +0100

    added TESTIMONIALS

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,18 @@
 $(function(){
     
-    
-    
     $('.popular__categories-btn').on('click', function(){ 
         $('.popular__categories-btn').removeClass('popular__categories-btn--active');
         $(this).addClass('popular__categories-btn--active');
     });
+
+    $('.testimonials__carousel').slick({
+        dots: true,
+        arrows: true,
+        focusOnSelect: true,
+        prevArrow: $('.slick-prev'),
+        nextArrow: $('.slick-next'),
+        appendArrows: $('.slick-dots'),
+      });
 
     $(".star").rateYo({
         starWidth: "40px"
